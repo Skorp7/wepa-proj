c54b5317e660906d0685fda9208cbb31d0153026
Sanna <sanna.korpi@live.fi>
2021-11-01 13:42:26 +0200

    postrge dependency

--- src/main/java/projekti/MyApplication.java
+++ src/main/java/projekti/MyApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class MyApplication {
-    @Profile("production")
+
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class);
     }

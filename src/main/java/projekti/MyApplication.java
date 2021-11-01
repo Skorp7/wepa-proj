eaf1403cffd6ab186c1ceab5dce18e00bafcf0eb
Sanna <sanna.korpi@live.fi>
2021-11-01 13:38:06 +0200

    css and profile fix

--- src/main/java/projekti/MyApplication.java
+++ src/main/java/projekti/MyApplication.java
@@ -2,12 +2,13 @@ package projekti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
+
 
 @SpringBootApplication
 public class MyApplication {
-
+    @Profile("production")
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class);
     }
-
 }
